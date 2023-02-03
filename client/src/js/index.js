5654d45f15769208c3362ea07f52e0e68888f15c
LRicciardo <liane.ricciardo@gmail.com>
2023-02-03 17:30:04 -0600

    worked on package.json/ worked on manifest/ comments in index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,11 +1,18 @@
+// >>> External modules >>>
+// a set of modules that are intended to run in the window context, which is to say, inside of your web pages.
 import { Workbox } from 'workbox-window';
+
+// >>> Internal modules >>>
+// export Editor class
 import Editor from './editor';
+// includes all exports to update the database and the stylesheet
 import './database';
 import '../css/style.css';
 
 const main = document.querySelector('#main');
 main.innerHTML = '';
 
+// load the spinning elements
 const loadSpinner = () => {
   const spinner = document.createElement('div');
   spinner.classList.add('spinner');
@@ -17,16 +24,19 @@ const loadSpinner = () => {
   main.appendChild(spinner);
 };
 
+// Create a new instance of the Editor class
 const editor = new Editor();
 
 if (typeof editor === 'undefined') {
+  // if no editor is defined, call the loadSpinner function
   loadSpinner();
 }
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  // register workbox service worker
+  // Create a new instance of the workbox service worker
   const workboxSW = new Workbox('/src-sw.js');
+  // register workbox service worker
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,95 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+// Must be BEFORE the WebpackPwaManifest in the plugins array
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// -WebpackPwaManifest - connects modularized applications while running in the browser, contains the loading and resiolving logic needed to connect these modules.
+// -Generates a 'manifest.json' for your Progressive Web Application, with auto icon resizing and fingerprinting support.
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
+// -Extracts CSS into separate files
+// -Creates a CSS file per JS file which contains CSS
+// -Supports On-Demand-Loading of CSS and SourceMaps
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// -Provides utilities for working with file and directory paths
+const path = require("path");
+
+// Inject Manifest will generate a manifest(list) of URLS to precache and add that list to the service worker file.
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    // sets the mode to development unless explicitly overridden by the package.json script
+    mode: "development",
+    // entry points using Object Syntax. Each entry point can have attributes. Here are 2 entry points: main and install
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js"
     },
+    // what to name the bundle file and where to write put it (ONLY ONE)
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "jate-bundle.js",
+      path: path.resolve(__dirname, "dist")
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATEditor"
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: "./src/sw.js",
+        swDest: "src-service-worker.js"
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Text Editor',
+        // background_color: '#21b649',
+        background_color: '#218EB6',
+        theme_color: '#21b649',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
+      // Defining the files, applies loaders and types
       rules: [
-        
-      ],
-    },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource"
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime"
+              ]
+            }
+          }
+        }
+      ]
+    }
   };
 };
